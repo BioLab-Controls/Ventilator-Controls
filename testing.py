0a47d6c88042ba567c91664e72e4db9da668aba5
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2022-04-26 23:30:06 -0400

    added testing

---
+++ testing.py
@@ -0,0 +1,41 @@
+from wsgiref.simple_server import sys_version
+import time
+import nidaqmx
+from nidaqmx.constants import(LineGrouping)
+
+def PWM(values, motor, runtime):
+
+	toggle = True
+	
+	task_PWM = nidaqmx.Task()
+	
+	if motor == 0:
+		task_PWM.do_channels.add_do_chan("Dev1/port0/line0")
+	else:
+		task_PWM.do_channels.add_do_chan("Dev1/port0/line1")
+	
+	task_PWM.start()
+	
+	time_init = time.time()
+	
+	while toggle:
+		
+		for i in range(0, 255):
+			if i < values:
+				task_PWM.write(True)
+			else:
+				task_PWM.write(False)
+				
+		i = 0
+
+		if ((time.time() - time_init) >= runtime):
+			task_PWM.write(False)
+			toggle = False
+			task_PWM.stop
+			task_PWM.close()
+
+motor = input("Enter motor: ")
+
+timeD = input("Enter Time: ")
+
+PWM(245,motor,timeD)
