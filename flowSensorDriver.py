cfb3bd00e38ad579743bff352d77622946bf78a6
SoPurdy <purdy_clay@outlook.com>
2022-04-26 22:21:05 -0400

    updated sensor driver

--- flowSensorDriver.py
+++ flowSensorDriver.py
@@ -1,20 +1,19 @@
 import nidaqmx
 from nidaqmx.constants import LineGrouping
-import time
-import numpy as np
+#import time
+#import numpy as np
 
 def dataCollect(values,flowOn,runtime):
     toggle = True
     task_data = nidaqmx.Task()
     portA = "Dev1/port0/line6" # Counter
-    portB = "Dev1/port0/line7" # Frequency
+    #portB = "Dev1/port0/line7" # Frequency
     task_data.ci_channels.add_ci_count_edges_chan(portA)
-    task_data.ci_channels.add_ci_count_edges_chan(portB)
+    #task_data.ci_channels.add_ci_count_edges_chan(portB)
 
     task_data.start()
-    count = task_data.read(portA)
-    freq = task_data.read(portB)
+    freq = task_data.read(portA)
+    #freq = task_data.read(portB)
+    task_data.close()
 
-    return count
-    return freq
-    
+    return freq    
