21fc9c5542ae563a24b6aa865f9b9510771dee0a
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2022-05-20 15:49:50 -0400

    folder

---
+++ Raspberry_PI/main.py
@@ -0,0 +1,23 @@
+import time
+from tkinter import N
+import RPi.GPIO as GPIO
+
+def main():
+    vsl = 0
+
+
+
+
+def switch_valves():
+    vss = 0
+
+
+
+def engagePump(pumpNo, time):LL
+    if pumpNo == 0:
+        for duty in range(0,101,1):
+            pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+            sleep(0.01)
+    
+
+main()
