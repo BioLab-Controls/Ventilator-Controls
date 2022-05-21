0c574003fb11208c6ab6fafb20874a1f4cd8772c
jake161 <jacob.browning161@gmail.com>
2022-05-21 20:54:48 +0100

    Added valve Control

--- Raspberry_PI/ventilator.py
+++ Raspberry_PI/ventilator.py
@@ -1,9 +1,17 @@
-from rpi_hardware_pwm import HardwarePWM as hPWM
+from rpi_hardware_pwm import HardwarePWM as hPWM 
+#using this instead of RPi.GPIO due to sPWM instead of hPWM, heard issues of pins not turning off if thats true we'll burn that bridge when we come to it
+import RPi.GPIO as GPIO
 from time import sleep
 
 #using ssh from VSCode into pi seems to eliminate the missing imports issue... good to know
 
 #some setup
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+
+
 dc=0 #initial duty cycle value of 0%
 
 pwm0=hPWM(pwm_channel=0,hz=1000)
@@ -20,8 +28,16 @@ def engagePump(pumpNo, time, duty):
         pwm1.change_duty_cycle(duty)
         sleep(time)
 
+def valveControl(valve, state, time):
+    GPIO.output(valve, state)
+    sleep(time)
+    GPIO.output(valve, False)
+
+
 try:
-    engagePump(1,10,50)
+    engagePump(1,10,50) #these values are arbitrary
+    valveControl(3,True,5)
+    valveControl(5,True,5)
 
 except KeyboardInterrupt:
     print ("\nTerminating Program")
