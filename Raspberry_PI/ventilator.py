59f8551b91c8beac8c265ce401a581bb77e6f9f7
jake161 <jacob.browning161@gmail.com>
2022-05-21 19:19:37 +0100

    Solved the missing dependencies issue

--- Raspberry_PI/ventilator.py
+++ Raspberry_PI/ventilator.py
@@ -2,4 +2,4 @@ from rpi_hardware_pwm import HardwarePWM as hPWM
 from time import sleep
 import RPi.GPIO as GPIO
 
-
+#using ssh from VSCode into pi seems to eliminate the missing imports issue... good to know
