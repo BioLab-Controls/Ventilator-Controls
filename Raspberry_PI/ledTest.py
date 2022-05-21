85b454cf3fcceddfcc37e6050b1dee7779a5a099
jake161 <jacob.browning161@gmail.com>
2022-05-21 13:36:02 -0400

    Simplified Test Code

--- Raspberry_PI/ledTest.py
+++ Raspberry_PI/ledTest.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
-RELAIS_1_GPIO = 18
-GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)
-pwm=GPIO.PWM(RELAIS_1_GPIO, 1000) #1kHz
+ledpin = 18
+GPIO.setup(ledpin, GPIO.OUT)
+pwm=GPIO.PWM(ledpin, 1000) #1kHz
 counter = 0
 dc=0
 pwm.start(dc)
@@ -12,11 +12,11 @@ pwm.start(dc)
 try:
 	for dc in range(0,101,5):
 		pwm.ChangeDutyCycle(dc)
-		time.sleep(.05)
+		sleep(.05)
 		print (dc)
 	for dc in range(95, 0, -5):    # Loop 95 to 5 stepping dc down by 5 each loop
 		pwm.ChangeDutyCycle(dc)
-		time.sleep(0.05)             # wait .05 seconds at current LED brightness
+		sleep(0.05)             # wait .05 seconds at current LED brightness
 		print(dc)
 
 	print ("done!")
