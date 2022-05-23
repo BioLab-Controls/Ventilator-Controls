b5aa6b9bdeeb72e8027c4cefbccd0c0e207ae01a
jake161 <jacob.browning161@gmail.com>
2022-05-23 20:34:58 +0100

    More sPWM updates

--- Raspberry_PI/ventilatorsPWM.py
+++ Raspberry_PI/ventilatorsPWM.py
@@ -5,10 +5,33 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
+GPIO.setup(35, GPIO.OUT)
 
-p=GPIO.PWM(12, 50)
+p=GPIO.PWM(12, 800)
+p1=GPIO.PWM(35, 800)
 
 p.start(0)
+p1.start(0)
+
+def engagePump():#channel, dc):
+    """Engage Pumps
+    Channel 0 or 1
+    0<=dc<=100
+    """
+    """
+    if channel==0:
+        p.ChangeDutyCycle(dc)
+    elif channel==1:
+        p1.ChangeDutyCycle(dc)
+    """
+    for dc in range(0, 101, 5):
+        p.ChangeDutyCycle(dc)
+        p1.ChangeDutyCycle(dc)
+        time.sleep(0.1)
+    for dc in range(100, -1, -5):
+        p.ChangeDutyCycle(dc)
+        p1.ChangeDutyCycle(dc)
+        time.sleep(0.1)
 try:
     """
     while 1:
@@ -18,10 +41,17 @@ try:
         for dc in range(100, -1, -5):
             p.ChangeDutyCycle(dc)
             time.sleep(0.1)
-    """
+    
     GPIO.output(11, GPIO.HIGH)
     GPIO.output(13, GPIO.HIGH)
     time.sleep(5)
+    """
+    engagePump()
+    engagePump()
+    GPIO.output(11,GPIO.HIGH)
+    GPIO.output(13,GPIO.HIGH)
+    time.sleep(5)
+
 except KeyboardInterrupt:
     print("\n")
 
