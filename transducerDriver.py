a6c3e445a550ad944c1f4b06d109f9f8a920ea95
Jacob B <62108812+jake161@users.noreply.github.com>
2022-05-25 18:58:08 -0400

    I fixed your spaghetti code :P

--- transducerDriver.py
+++ transducerDriver.py
@@ -1,29 +1,41 @@
 import numpy as np
-from scipy.interpolate import interp1d
 import nidaqmx
 from nidaqmx.constants import LineGrouping
 
+#setup
+pSenTask = nidaqmx.Task()
+port = "Dev1/ai0"
+pSenTask.ai_channels.add_ai_voltage_chan(port)
+pSenTask.start()
+
 def pressureTransducer():
     #Create task 
-    pSenTask = nidaqmx.Task()
+    #pSenTask = nidaqmx.Task()
     #IO
-    port = "Dev1/ai2"
-    pSenTask.ai_channels.add_ai_voltage_chan(port)
-    pSenTask.start()
+    #port = "Dev1/ai0"
+    #pSenTask.ai_channels.add_ai_voltage_chan(port)
+    #pSenTask.start()
     dataIN = pSenTask.read()
     linearconvertPSIG = np.interp(256,[5,1000],[0,5])
     #Map values using calibration function (to psia)
     calib_convertPSIG = (150.18 * dataIN) + 0.1156
     #convertcmH20 = m * 70.307
-    pSenTask.stop
-    pSenTask.close()
-    print(calib_convertPSIG)
-    #return calib_convertPSIG
+    #pSenTask.stop
+    #pSenTask.close()
+    #print(calib_convertPSIG)
+    return calib_convertPSIG
 
 
 def loopData():
     while True:
         print(pressureTransducer())
 
+try:
+    loopData()
 
-loopData();
+except KeyboardInterrupt:
+    print("INTERUPTED")
+
+finally:
+    pSenTask.stop
+    pSenTask.close()
