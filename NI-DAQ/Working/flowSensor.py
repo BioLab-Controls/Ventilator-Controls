3ab8dfb9496d7e4547aefcf63449f5f78ee8b800
SoPurdy <purdy_clay@outlook.com>
2022-05-26 15:15:02 -0400

    changed back to ci

--- NI-DAQ/Working/flowSensor.py
+++ NI-DAQ/Working/flowSensor.py
@@ -1,24 +1,24 @@
-from curses import keyname
 import nidaqmx
 from nidaqmx.constants import LineGrouping
 import time
 import numpy as np
 
+toggle = True
 task_data = nidaqmx.Task()
 port = "Dev1/port0/line6"
 
 def dataCollect():
-    toggle = True
+    
     while toggle:
-        task_data.di_channels.add_di_chan(port)
+
+        task_data.ci_channels.add_ci_count_edges_chan(port,initial_count=0)
         task_data.start()
         freq = task_data.read(port)
+        
 
     return freq
 
 try:
     dataCollect()
 except KeyboardInterrupt:
-    print("INTERRUPT")
-finally:
     task_data.close()
