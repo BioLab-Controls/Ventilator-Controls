73591ce8ebd912cbfcdbed1a9e1cc348e9f22756
Jacob B <62108812+jake161@users.noreply.github.com>
2022-05-26 15:46:28 -0400

    Well... the counter works :]

--- NI-DAQ/Working/flowSensor.py
+++ NI-DAQ/Working/flowSensor.py
@@ -1,19 +1,20 @@
 import nidaqmx
-from nidaqmx.constants import LineGrouping
-import time
-import numpy as np
+#import time
 
 toggle = True
 task_data = nidaqmx.Task()
-port = "Dev1/PFI/line0"
+port = "/Dev1/ctr0"
+task_data.ci_channels.add_ci_count_edges_chan(port)#,initial_count=0)
+task_data.ci_channels[0].ci_count_edges_term="/Dev1/PFI0"
+task_data.start()
 
+#it works
 def dataCollect():
     
     while toggle:
 
-        task_data.ci_channels.add_ci_count_edges_chan(port,initial_count=0)
-        task_data.start()
-        freq = task_data.read(port)
+        freq = task_data.read()
+        print(freq)
         
 
     return freq
@@ -21,4 +22,6 @@ def dataCollect():
 try:
     dataCollect()
 except KeyboardInterrupt:
+    print("INTERRUPTED")
+finally:
     task_data.close()
